605a247486111b6f1a75908ae5525fbae31bb062
Tristan Bull <tristan.bull@avalara.com>
2022-11-03 11:52:32 -0700

    Adding digital input example.

---
+++ avr-digitalin/src/main.rs
@@ -0,0 +1,30 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+
+    /*
+     * For examples (and inspiration), head to
+     *
+     *     https://github.com/Rahix/avr-hal/tree/main/examples
+     *
+     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
+     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
+     * examples available.
+     */
+
+    let mut button = pins.d12.into_pull_up_input();
+
+    loop {
+        let button_state = button.is_high();
+        ufmt::uwriteln!(&mut serial, "Button state is '{}'", button_state);
+        arduino_hal::delay_ms(1000);
+    }
+}
